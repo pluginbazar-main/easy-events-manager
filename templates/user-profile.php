07ff5468b8f5030065c66579d72061b742f7ddfc
Farhad Ali <mycodrops@gmail.com>
2019-09-18 18:09:18 +0600

    schedule page problem fix

--- templates/user-profile.php
+++ templates/user-profile.php
@@ -16,8 +16,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     <div class="eem-force-full-width eem-user-banner">
         <div class="pb-container">
             <div class="btn-control">
-                <a href="#" class="btn-edit">Edit Profile</a>
-                <a href="#" class="btn-logout">Logout</a>
+                <a href="#" class="btn-edit"><i class="icofont-edit-alt"></i> Edit Profile</a>
+                <a href="#" class="btn-logout"><i class="icofont-logout"></i> Logout</a>
             </div>
         </div>
     </div>
@@ -111,8 +111,11 @@ if ( ! defined( 'ABSPATH' ) ) {
                             <div class="tab-content">
 
                                 <div class="tab-item-content active">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
-                                        atque dolore dolorem ex fuga molestiae provident</p>
+                                    <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
+                                    <p>I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents.</p>
+                                    <p>I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p>
+                                    <p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies.</p>
                                 </div>
 
                             </div>
