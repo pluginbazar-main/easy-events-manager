5ebfae5cf14447e75d341c1622f08cd02ea132e1
Farhad Ali <mycodrops@gmail.com>
2019-09-22 12:48:46 +0600

    Archive default search form design

--- templates/loop/search.php
+++ templates/loop/search.php
@@ -10,5 +10,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+
+$search_fields = array(
+	array(
+		'id'          => 'sf_keyword',
+		'placeholder' => esc_html__( 'technology', EEM_TD ),
+		'type'        => 'text',
+	),
+);
+$search_fields = array( array( 'options' => apply_filters( 'eem_filters_archive_search_fields', $search_fields ) ) );
+
 ?>
-Search from main Plugin
+
+<div class="eem-force-full-width eem-default-search-form">
+    <div class="pb-container">
+        <div class="eem-event-search-wrap">
+            <h2 class="eem-event-search-title">Find your Event</h2>
+            <form class="eem-event-search-form">
+                <div class="form-fields">
+                    <input type="text" name="sf_keyword" placeholder="Keyword to search" autocomplete="off">
+                    <button class="eem-btn"><i class="icofont-search"></i></button>
+                </div>
+                <p class="eem-search-total-count">There are over 143 Events for you.</p>
+            </form>
+        </div>
+    </div>
+</div>
